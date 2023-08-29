5b68e596ff4e81a7ff688d84c968d8bdedc1beb6
Petrrrr573 <65619514+Petrrrr573@users.noreply.github.com>
2023-08-29 12:05:17 +0200

    bit of raycasting (horizontal walls when ray is facing up

--- GAME-SDL/main.cpp
+++ GAME-SDL/main.cpp
@@ -1,24 +1,37 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <iostream> 
+#include <cmath>
+#include <math.h>
 
 #define WIDTH 800
 #define HEIGHT 800
 
 #define PI 3.1415926535
 
+#define PI2 PI/2
+#define PI3 3*PI/2
 
-int mapX = 8, mapY = 8, mapS = 64;
+
+int mapX = 16, mapY = 16, mapS = 256;
 
 int map[] = {
-	1,1,1,1,1,1,1,1,
-	1,0,0,0,0,0,0,1,
-	1,0,0,0,0,0,0,1,
-	1,0,0,0,0,0,0,1,
-	1,0,0,1,0,0,1,1,
-	1,0,0,0,0,0,1,1,
-	1,0,0,0,0,0,1,1,
-	1,0,0,0,0,0,1,1,
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,1,1,0,1,1,0,0,0,0,0,1,
+	1,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,2,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,2,2,2,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 };
 
 double xPos = 100;
@@ -35,6 +48,12 @@ void DrawMap(SDL_Renderer* renderer, int mapX, int mapY, int mapS, int* map, SDL
 		for (x = 0; x < mapX; x++) {
 			if (map[y * mapX + x] == 1) {
 				SDL_SetRenderDrawColor(renderer, 35, 35, 35, 255);
+				SDL_Rect rect = { x * (mapS / mapX) + 1, y * (mapS / mapY) + 1, mapS / mapX - 1, mapS / mapY - 1 };
+				SDL_RenderFillRect(renderer, &rect);
+			}
+
+			if (map[y * mapX + x] == 2) {
+				SDL_SetRenderDrawColor(renderer, 0, 150, 0, 255);
 				SDL_Rect rect = { x * (mapS / mapX) + 1, y * (mapS / mapY) + 1, mapS / mapX - 1, mapS / mapY - 1 };
 				SDL_RenderFillRect(renderer, &rect);
 			}
@@ -59,8 +78,13 @@ void DrawMap(SDL_Renderer* renderer, int mapX, int mapY, int mapS, int* map, SDL
 }
 
 void Input(bool& isRunning, int player_speed, int w_tille, int mapX, int mapY, int mapS, int* map, double& xPos, double& yPos, double& pdx, double& pdy, double& pa, int PLAYER_WIDTH, int PLAYER_HEIGHT, int& currentFrame, double RAD_PER_FRAME) {
-	if (currentFrame >= 32) { currentFrame = 0; }
-	else if (currentFrame < 0) { currentFrame = 31; }
+	if (currentFrame == 0) {
+		pa = 0;
+	} else if (currentFrame >= 32) {
+		currentFrame = 0;
+	} else if (currentFrame < 0) {
+		currentFrame = 31;
+	}
 
 	SDL_Event event;
 
@@ -111,18 +135,273 @@ void Input(bool& isRunning, int player_speed, int w_tille, int mapX, int mapY, i
 	}
 }
 
-void raycasting(int mapX, int mapY, int mapS, int* map, double xPos, double yPos, double pa, SDL_Renderer* renderer) {
-	
+void raycasting(int mapX, int mapY, int mapS, int* map, double xPos, double yPos, double pa, SDL_Renderer* renderer, int w_tille, int& currentFrame) {
+	double ry = 0, rx = 0;
+	double xo = 0, yo = 0;
+
+	double px = xPos + 16;
+	double py = yPos + 16;
+
+	double dx=0, dy = 0;
+
+	int mx, my, mp;
+
+	int dof;
+
+	double ra = pa;
+
+	ra -= 0.0872664626*4;
+	for (int r = 0; r < 10; r++) {
+		ra += 0.0174532925 * 4;
+		if (ra < 0) {
+			ra += 2 * PI;
+		}
+		if (ra > 2 * PI) {
+			ra -= 2 * PI;
+		}
+		// Check horizontal lines
+		dof = 0;
+		double aTan = -1 /tan(ra);
+
+		// looking straight left or right
+		if (ra <= 0 || ra == PI) {
+			rx = px; ry = py;
+			dof = 8;
+		}
+
+		std::cout << ra << " " << currentFrame << std::endl;
+
+		// looking up
+		if (ra > PI) {
+			ry = floor(py / w_tille) * w_tille;
+			dy = py - ry;
+			rx = (dy) * aTan + px;
+			yo = -w_tille;
+			xo = -yo * aTan;
+			if (rx > 0 && rx < 800) {
+				if (ry > 0 && ry < 800) {
+					if (r == 8) {
+						SDL_SetRenderDrawColor(renderer, 255, 105, 180, 255);
+						SDL_RenderDrawLine(renderer, px, py, rx, ry);
+						SDL_Rect ray_rect = { rx - 5, ry - 5, 10, 10};
+						SDL_RenderDrawRect(renderer, &ray_rect);
+
+					}
+					else {
+						SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+						SDL_RenderDrawLine(renderer, px, py, rx, ry);
+					}
+					SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+					SDL_RenderDrawLine(renderer, px, py, px, ry);
+					SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+					SDL_RenderDrawLine(renderer, px, ry, rx, ry);
+				}
+			}
+		}
+
+		// looking down
+		/*if (ra < PI) {
+			ry = floor(py / w_tille) * w_tille + w_tille;
+			dy = py - ry;
+			rx = (dy) * aTan + px;
+			yo = w_tille;
+			xo = -yo * aTan;
+		}*/
+
+		//while (dof < 8) {
+		//	mx = int(floor(rx / w_tille));
+		//	my = int(floor(ry / w_tille));
+		//	mp = my * mapX + mx;
+
+		//	// hit wall
+		//	if (mp > 0 && mp < mapX * mapY && map[mp] == 1) {
+		//		dof = 8;
+		//	}
+		//	else {
+		//		dof++;
+		//		rx += xo;
+		//		ry += yo;
+		//	}
+		//}
+
+		//if (rx > 0 && rx < 800) {
+		//	if (ry > 0 && ry < 800) {
+		//		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		//		SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, rx, ry);
+		//	}
+		//}
 
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-	SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, 0, 0);
+		//ra -= 0.0872664626 * 2;
+		//for (int r = 0; r < 10; r++) {
+		//	// Check horizontal lines
+		//	dof = 0;
+		//	double aTan = -1 / tan(ra);
+
+		//	// looking up
+		//	if (ra > PI) {
+		//		ry = floor(py / w_tille) * w_tille;
+		//		dy = py - ry;
+		//		rx = (dy)*aTan + px;
+		//		yo = -w_tille;
+		//		xo = -yo * aTan;
+		//	}
+
+		//	// looking down
+		//	//if (ra < PI) {
+		//	//	ry = floor(py / w_tille) * w_tille + w_tille;
+		//	//	dy = py - ry;
+		//	//	rx = (dy) * aTan + px;
+		//	//	yo = w_tille;
+		//	//	xo = -yo * aTan;
+		//	//}
+
+		//	//// looking straight left or right
+		//	//if (ra == 0 || ra == PI) {
+		//	//	rx = xPos+16; ry = yPos+16;
+		//	//	dof = 8;
+		//	//}
+
+		//	while (dof < 8) {
+		//		mx = int(floor(rx / w_tille));
+		//		my = int(floor(ry / w_tille));
+		//		mp = my * mapX + mx;
+
+		//		// hit wall
+		//		if (mp > 0 && mp < mapX * mapY && map[mp] == 1) {
+		//			std::cout << "zed" << std::endl;
+		//			if (rx > 0 && rx < 800) {
+		//				if (ry > 0 && ry < 800) {
+		//					SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		//					SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, rx, ry);
+		//				}
+		//			}
+		//			dof = 8;
+		//		}
+		//		else {
+		//			dof++;
+		//			rx += xo;
+		//			ry += yo;
+		//		}
+
+		//	}
+
+		//	if (dof > 7) {
+		//		if (rx > 0 && rx < 800) {
+		//			if (ry > 0 && ry < 800) {
+		//				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		//				SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, rx, ry);
+		//			}
+		//		}
+		//	}
+
+		//	ra += 0.0174532925 * 2;
+
+		//// looking down
+		//if (ra < PI) {
+		//	ry = yPos - (floor((yPos+16) / w_tille+1) * w_tille) + w_tille;
+		//	rx = (yPos+16 - ry) * aTan + xPos+16;
+		//	yo = w_tille;
+		//	xo = -yo * aTan;
+		//}
+
+		//// looking straight left or right
+		//if (ra == 0 || ra == PI) {
+		//	rx = xPos+16; ry = yPos+16;
+		//	dof = 8;
+		//}
+
+
+		// Check vertical lines
+		//dof = 0;
+		//double nTan = -tan(ra);
+
+
+		//// looking left
+		//if (ra > PI2 && ra<PI3) {
+		//	rx = xPos - (floor((xPos + 16) / w_tille) * w_tille) - 0.0001;
+		//	ry = (xPos + 16 - ry) * nTan + yPos + 16;
+		//	xo = -w_tille;
+		//	yo = -xo * nTan;
+		//}
+
+		//// looking right
+		//if (ra < PI2 || ra>PI3) {
+		//	rx = xPos - (floor((xPos + 16) / w_tille) * w_tille) + w_tille;
+		//	ry = (xPos + 16 - ry) * nTan + yPos + 16;
+		//	xo = w_tille;
+		//	yo = -xo * nTan;
+		//}
+
+		//// looking straight up or down
+		//if (ra == 0 || ra == PI) {
+		//	rx = xPos; ry = yPos;
+		//	dof = 8;
+		//}
+
+		//while (dof < 8) {
+		//	mx = int(floor(rx / w_tille));
+		//	my = int(floor(ry / w_tille));
+		//	mp = my * mapX + mx;
+
+		//	// hit wall
+		//	if (mp > 0 && mp < mapX * mapY && map[mp] == 1) {
+		//		dof = 8;
+		//	}
+		//	else {
+		//		rx += xo;
+		//		ry += yo;
+		//		dof++;
+		//	}
+		//}
+
+		//if (rx < 0) {rx = 0;}
+		//if (ry < 0) { ry = 0; }
+		//if (rx > WIDTH) { rx = WIDTH; }
+		//if (ry > HEIGHT) { ry = HEIGHT; }
+		//SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		//SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, rx, ry);
+
+
+
+		/*float dx = 0;
+		std::cout << la * 57.2957795 << std::endl;
+		if (la > 0 && (la + 0.0174532925) * 57.2957795 < 90) {
+			la += 0.0174532925;
+			if (la != PI / 2 && la != 3 * PI / 2) {
+				dx = dy / tan(la);
+			}
+			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+			SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, xPos + dx, yPos - dy);
+		}*/
+	}
+
+	/*for (int r = 0; r < 240; r++){
+		float yo = 0, xo = 0;
+		la += 0.00218166156;
+		while (map[int((floor((yPos+yo)/w_tille)) * mapX + int((floor((xPos+xo)/w_tille))))] == 0) {
+			float pdx = cos(la)*2;
+			float pdy = sin(la)*2;
+
+			yo += pdy;
+			xo += pdx;
+		}
+		if (map[int((floor((yPos + yo) / w_tille)) * mapX + int((floor((xPos + xo) / w_tille))))] == 1) {
+			SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+			SDL_RenderDrawLine(renderer, xPos+16, yPos+16, xPos+xo, yPos+yo);
+		}
+
+		if (map[int((floor((yPos + yo) / w_tille)) * mapX + int((floor((xPos + xo) / w_tille))))] == 2) {
+			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+			SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, xPos + xo, yPos + yo);
+		}
+	}*/
 }
 
 int main(int argc, char** argv){
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* window = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+	SDL_Window* window = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH*2, HEIGHT, 0);
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
@@ -130,7 +409,7 @@ int main(int argc, char** argv){
 
 	SDL_GetRendererOutputSize(renderer, &mapS, &mapS); // Gets the width of the screen
 
-	int w_tille = mapS / mapX; // Gets the width of a tille
+	int w_tille = WIDTH / mapX; // Gets the width of a tille
 
 
 	SDL_Rect player_rect = { 100,100,25,25};
@@ -150,8 +429,8 @@ int main(int argc, char** argv){
 	static int lastTime;
 
 	// Variables to store the player's position
-	pdx = cos(pa) * 5;
-	pdy = sin(pa) * 5;
+	pdx = cos(pa) * 3;
+	pdy = sin(pa) * 3;
 
 	SDL_Surface* playerSurface = IMG_Load("C:\\Users\\Petr\\Desktop\\player.png");
 	SDL_Texture* playerTexture = SDL_CreateTextureFromSurface(renderer, playerSurface);
@@ -180,9 +459,6 @@ int main(int argc, char** argv){
 
 		DrawMap(renderer, mapX, mapY, mapS, map, player_rect); // Draws the map
 
-		//raycasting(mapX, mapY, mapS, map, xPos, yPos, pa, renderer);
- 
-
 		 // Calculate the source rectangle based on the current frame
 		int srcX = round(currentFrame * PLAYER_WIDTH);
 		int srcY = 0; // Since all frames are on a single row, y is always 0
@@ -194,8 +470,10 @@ int main(int argc, char** argv){
 		// Render the player's current frame
 		SDL_RenderCopy(renderer, playerTexture, &srcRect, &destRect);
 
-		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-		SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, xPos + 16 + pdx*50, yPos + 16 + pdy*50);
+		/*SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+		SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, xPos + 16 + pdx*50, yPos + 16 + pdy*50);*/
+
+		raycasting(mapX, mapY, mapS, map, xPos, yPos, pa, renderer, w_tille, currentFrame);
 
 		SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 
