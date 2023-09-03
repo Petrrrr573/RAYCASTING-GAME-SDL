f72ced2cd102724fdf6c24a514bb9b4b38d9e80f
Petrrrr573 <65619514+Petrrrr573@users.noreply.github.com>
2023-09-03 10:43:11 +0200

    Some renaming for better readability

--- GAME-SDL/main.cpp
+++ GAME-SDL/main.cpp
@@ -12,11 +12,11 @@
 #define PI2 PI/2
 #define PI3 3*PI/2
 
-#define RAD 0.0174532925
+#define RAD 0.0174532925 // One degree in radiants
 
 class Game {
 public:
-	int mapX = 16, mapY = 16, mapS = 256;
+	int mapX = 16, mapY = 16, mapSize = 256;
 
 	int map[256] = {
 		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -38,7 +38,7 @@ public:
 	};
 
 	bool isRunning;
-	int w_tille;
+	int tilleWidth;
 
 	SDL_Window* window;
 	SDL_Renderer* renderer;
@@ -47,17 +47,17 @@ public:
 
 	int timerFPS;
 
-	void MakeWindow() {
+	void MakeWindow(const char* name) {
 		isRunning = true;
 
-		window = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH * 2, HEIGHT, 0);
+		window = SDL_CreateWindow(name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH * 2, HEIGHT, 0);
 
 		renderer = SDL_CreateRenderer(window, -1, 0);
 
 
-		SDL_GetRendererOutputSize(renderer, &mapS, &mapS); // Gets the width of the screen
+		SDL_GetRendererOutputSize(renderer, &mapSize, &mapSize); // Gets the width of the screen
 
-		w_tille = WIDTH / mapX; // Gets the width of a tille
+		tilleWidth = WIDTH / mapX; // Gets the width of a tille
 	}
 
 
@@ -69,13 +69,13 @@ public:
 			for (x = 0; x < mapX; x++) {
 				if (map[y * mapX + x] == 1) {
 					SDL_SetRenderDrawColor(renderer, 35, 35, 35, 255);
-					SDL_Rect rect = { x * (mapS / mapX) + 1, y * (mapS / mapY) + 1, mapS / mapX - 1, mapS / mapY - 1 };
+					SDL_Rect rect = { x * (mapSize / mapX) + 1, y * (mapSize / mapY) + 1, mapSize / mapX - 1, mapSize / mapY - 1 };
 					SDL_RenderFillRect(renderer, &rect); 
 				}
 
 				if (map[y * mapX + x] == 2) {
 					SDL_SetRenderDrawColor(renderer, 0, 150, 0, 255);
-					SDL_Rect rect = { x * (mapS / mapX) + 1, y * (mapS / mapY) + 1, mapS / mapX - 1, mapS / mapY - 1 };
+					SDL_Rect rect = { x * (mapSize / mapX) + 1, y * (mapSize / mapY) + 1, mapSize / mapX - 1, mapSize / mapY - 1 };
 					SDL_RenderFillRect(renderer, &rect);
 				}
 			}
@@ -84,18 +84,14 @@ public:
 		// Draw the horizontal grid lines
 		for (y = 0; y < mapY; y++) {
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			SDL_RenderDrawLine(renderer, 0, y * (mapS / mapY), mapS, y * (mapS / mapY));
+			SDL_RenderDrawLine(renderer, 0, y * (mapSize / mapY), mapSize, y * (mapSize / mapY));
 		}
 
 		// Draw the vertical grid lines
 		for (x = 0; x < mapX; x++) {
-			SDL_RenderDrawLine(renderer, x * (mapS / mapX), 0, x * (mapS / mapX), mapS);
+			SDL_RenderDrawLine(renderer, x * (mapSize / mapX), 0, x * (mapSize / mapX), mapSize);
 		}
 
-
-		/*SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderFillRect(renderer, &player_rect);*/
-		//SDL_SetRenderDrawColor(renderer, 125, 125, 125, 255);
 	}
 
 	void HandleFps() {
@@ -114,7 +110,240 @@ public:
 		}
 	}
 	
-	
+	double distance(double playerX, double playerY, double rayX, double rayY, double rayAngle, double playerAngle) {
+		double distance = 0;
+
+		double x = rayX - playerX;
+		double y = rayY - playerY;
+
+		distance = x * cos(playerAngle) + y * sin(playerAngle);
+
+		return distance;
+	}
+
+	void raycasting(double xPos, double yPos, double playerAngle, int& currentFrame) {
+		double rayY = -1, rayX = -1;
+
+		double hy = 0, hx = 0;
+		double vy = 0, vx = 0;
+		double hd = 0, vd = 0;
+
+		double fd = 0;
+
+		double xo = 0, yo = 0;
+
+		double playerX = xPos + 16;
+		double playerY = yPos + 16;
+
+		double dx = 0, dy = 0;
+
+		int mx, my, mp;
+
+		int dof;
+
+		double rayAngle = playerAngle;
+
+		int rays = 100;
+
+		rayAngle -= rays / 2 * RAD;
+
+
+		SDL_SetRenderDrawColor(renderer, 200, 20, 20, 255);
+		SDL_Rect rect = { WIDTH, 800, WIDTH, -HEIGHT / 2 };
+		SDL_RenderFillRect(renderer, &rect);
+
+		for (int r = 0; r < rays; r++) {
+			bool horizontalHit;
+			if (rayAngle < 0) {
+				rayAngle += 2 * PI;
+			}
+			if (rayAngle > 2 * PI) {
+				rayAngle -= 2 * PI;
+			}
+			// Check horizontal lines
+			dof = 0;
+			double aTan = -1 / tan(rayAngle);
+
+			// looking straight left or right
+			if (rayAngle <= 0 || rayAngle == PI) {
+				rayX = playerX; rayY = playerY;
+				dof = 30;
+			}
+
+			// looking up
+			if (rayAngle > PI) {
+				rayY = floor(playerY / tilleWidth) * tilleWidth - 1;
+				dy = playerY - rayY;
+				rayX = (dy)*aTan + playerX;
+				yo = -tilleWidth;
+				xo = -yo * aTan;
+			}
+
+			// looking down
+			if (rayAngle < PI) {
+				rayY = floor(playerY / tilleWidth) * tilleWidth + tilleWidth;
+				dy = playerY - rayY;
+				rayX = (dy)*aTan + playerX;
+				yo = tilleWidth;
+				xo = -yo * aTan;
+			}
+
+
+			while (dof < 30) {
+				mx = int(floor(rayX / tilleWidth));
+				my = int(floor(rayY / tilleWidth));
+				mp = my * mapX + mx;
+
+				// hit wall
+				if (mp > 0 && mp < mapX * mapY && map[mp] != 0) {
+					dof = 30;
+					break;
+				}
+				else {
+					dof++;
+					rayX += xo;
+					rayY += yo;
+				}
+			}
+
+			hy = rayY, hx = rayX;
+			hd = distance(playerX, playerY, hx, hy, rayAngle, playerAngle);
+
+			// Check vertical lines
+
+			dof = 0;
+			double nTan = -tan(rayAngle);
+
+			// looking straight up or down
+			if (rayAngle == PI3 || rayAngle == PI2) {
+				rayX = playerX; rayY = playerY;
+				dof = 30;
+			}
+
+			// looking right
+			if (rayAngle < PI2 || rayAngle > PI3) {
+				rayX = floor(playerX / tilleWidth) * tilleWidth + tilleWidth;
+				dx = playerX - rayX;
+				rayY = (dx)*nTan + playerY;
+				xo = tilleWidth;
+				yo = -xo * nTan;
+			}
+
+			// looking left
+			if (rayAngle > PI2 && rayAngle < PI3) {
+				rayX = floor(playerX / tilleWidth) * tilleWidth - 1;
+				dx = playerX - rayX;
+				rayY = (dx)*nTan + playerY;
+				xo = -tilleWidth;
+				yo = -xo * nTan;
+			}
+
+			while (dof < 30) {
+				mx = int(floor(rayX / tilleWidth));
+				my = int(floor(rayY / tilleWidth));
+				mp = my * mapX + mx;
+
+				// hit wall
+				if (mp > 0 && mp < mapX * mapY && map[mp] != 0) {
+					dof = 30;
+					break;
+				}
+				else {
+					dof++;
+					rayX += xo;
+					rayY += yo;
+				}
+			}
+
+			vy = rayY, vx = rayX;
+			vd = distance(playerX, playerY, vx, vy, rayAngle, playerAngle);
+
+			if (hd > vd) {
+				rayX = vx;
+				rayY = vy;
+				fd = vd;
+				horizontalHit = false;
+			}
+			else if (hd < vd) {
+				rayX = hx;
+				rayY = hy;
+				fd = hd;
+				horizontalHit = true;
+			}
+			else {
+				// Both distances are the same; handle corner/edge case
+				// You can choose which wall to render based on ray direction
+				if (rayAngle >= 0 && rayAngle < PI) {
+					// Ray is facing down, prefer vertical hit
+					rayX = hx;
+					rayY = hy;
+					fd = hd;
+					horizontalHit = true;
+				}
+				else {
+					// Ray is facing up, prefer horizontal hit
+					rayX = vx;
+					rayY = vy;
+					fd = vd;
+					horizontalHit = false;
+				}
+			}
+
+			if (rayX >= 0 && rayX <= 800) {
+				if (rayY >= 0 && rayY <= 800) {
+					mx = int(floor(rayX / tilleWidth));
+					my = int(floor(rayY / tilleWidth));
+					mp = my * mapX + mx;
+
+					// hit wall
+					if (mp > 0 && mp < mapX * mapY) {
+						if (map[mp] == 1) {
+							SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+						}
+						else if (map[mp] == 2) {
+							SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+						}
+						else {
+							SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+						}
+						SDL_RenderDrawLine(renderer, playerX, playerY, rayX, rayY);
+					}
+
+					// Draw 3D Walls
+					double wh = (tilleWidth * HEIGHT) / fd;
+					if (wh > 1000) {
+						wh = 1000;
+					}
+
+					double lineO = HEIGHT - wh / 1.5;
+
+					SDL_Rect rect = { r * (800 / rays) + WIDTH, lineO / 2, 800 / rays, wh };
+					if (mp > 0 && mp < mapX * mapY) {
+						if (map[mp] == 1) {
+							if (horizontalHit == true) {
+								SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+							}
+							else if (horizontalHit == false) {
+								SDL_SetRenderDrawColor(renderer, 0, 0, 130, 255);
+							}
+							else { continue; }
+						}
+						else if (map[mp] == 2) {
+							if (horizontalHit == true) {
+								SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+							}
+							else if (horizontalHit == false) {
+								SDL_SetRenderDrawColor(renderer, 0, 130, 0, 255);
+							}
+						}
+						SDL_RenderFillRect(renderer, &rect);
+					}
+				}
+			}
+
+			rayAngle += RAD;
+		}
+	}
 };
 
 
@@ -122,7 +351,7 @@ class Player {
 public:
 	double xPos = 100;
 	double yPos = 100;
-	double pa;
+	double playerAngle;
 
 	SDL_Rect player_rect = { 100,100,25,25 };
 	int player_speed = 6;
@@ -138,8 +367,8 @@ public:
 	int currentFrame = 0;
 
 	// Variables to store the player's position
-	double pdx = cos(pa) * 3;
-	double pdy = sin(pa) * 3;
+	double pdx = cos(playerAngle) * 3;
+	double pdy = sin(playerAngle) * 3;
 
 	SDL_Surface* playerSurface;
 	SDL_Texture* playerTexture;
@@ -150,7 +379,7 @@ public:
 		SDL_FreeSurface(playerSurface);
 	}
 
-	void Input(bool& isRunning, int w_tille, int mapX, int mapY, int mapS, int* map) {
+	void Input(bool& isRunning, int tilleWidth, int mapX, int mapY, int mapSize, int* map) {
 		if (currentFrame >= 32) {
 			currentFrame = 0;
 		}
@@ -170,38 +399,38 @@ public:
 
 		if (state[SDL_SCANCODE_A]) {
 			currentFrame += 1;
-			pa -= RAD_PER_FRAME / 2;
-			if (pa < 0) {
-				pa += 2 * PI;
+			playerAngle -= RAD_PER_FRAME / 2;
+			if (playerAngle < 0) {
+				playerAngle += 2 * PI;
 			}
-			pdx = cos(pa) * 3;
-			pdy = sin(pa) * 3;
+			pdx = cos(playerAngle) * 3;
+			pdy = sin(playerAngle) * 3;
 		}
 
 		if (state[SDL_SCANCODE_D]) {
 			currentFrame -= 1;
-			pa += RAD_PER_FRAME / 2;
-			if (pa > 2 * PI) {
-				pa -= 2 * PI;
+			playerAngle += RAD_PER_FRAME / 2;
+			if (playerAngle > 2 * PI) {
+				playerAngle -= 2 * PI;
 			}
-			pdx = cos(pa) * 3;
-			pdy = sin(pa) * 3;
+			pdx = cos(playerAngle) * 3;
+			pdy = sin(playerAngle) * 3;
 		}
 
 		if (state[SDL_SCANCODE_W]) {
-			if (map[int((floor(yPos + pdy) / w_tille)) * mapX + int(floor((xPos) / w_tille))] == 0 && map[int((floor(yPos + pdy) / w_tille)) * mapX + int(floor((xPos + 32) / w_tille))] == 0 && map[int((floor(yPos + pdy + 32) / w_tille)) * mapX + int(floor((xPos + 32) / w_tille))] == 0 && map[int((floor(yPos + pdy + 32) / w_tille)) * mapX + int(floor((xPos) / w_tille))] == 0) {
+			if (map[int((floor(yPos + pdy) / tilleWidth)) * mapX + int(floor((xPos) / tilleWidth))] == 0 && map[int((floor(yPos + pdy) / tilleWidth)) * mapX + int(floor((xPos + 32) / tilleWidth))] == 0 && map[int((floor(yPos + pdy + 32) / tilleWidth)) * mapX + int(floor((xPos + 32) / tilleWidth))] == 0 && map[int((floor(yPos + pdy + 32) / tilleWidth)) * mapX + int(floor((xPos) / tilleWidth))] == 0) {
 				yPos += pdy;
 			}
-			if (map[int((floor(yPos) / w_tille)) * mapX + int(floor((xPos + pdx) / w_tille))] == 0 && map[int((floor(yPos) / w_tille)) * mapX + int(floor((xPos + pdx + 32) / w_tille))] == 0 && map[int((floor(yPos + 32) / w_tille)) * mapX + int(floor((xPos + pdx + 32) / w_tille))] == 0 && map[int((floor(yPos + 32) / w_tille)) * mapX + int(floor((xPos + pdx) / w_tille))] == 0) {
+			if (map[int((floor(yPos) / tilleWidth)) * mapX + int(floor((xPos + pdx) / tilleWidth))] == 0 && map[int((floor(yPos) / tilleWidth)) * mapX + int(floor((xPos + pdx + 32) / tilleWidth))] == 0 && map[int((floor(yPos + 32) / tilleWidth)) * mapX + int(floor((xPos + pdx + 32) / tilleWidth))] == 0 && map[int((floor(yPos + 32) / tilleWidth)) * mapX + int(floor((xPos + pdx) / tilleWidth))] == 0) {
 				xPos += pdx;
 			}
 		}
 
 		if (state[SDL_SCANCODE_S]) {
-			if (map[int((floor(yPos - pdy) / w_tille)) * mapX + int(floor((xPos) / w_tille))] == 0 && map[int((floor(yPos - pdy) / w_tille)) * mapX + int(floor((xPos + 32) / w_tille))] == 0 && map[int((floor(yPos - pdy + 32) / w_tille)) * mapX + int(floor((xPos + 32) / w_tille))] == 0 && map[int((floor(yPos - pdy + 32) / w_tille)) * mapX + int(floor((xPos) / w_tille))] == 0) {
+			if (map[int((floor(yPos - pdy) / tilleWidth)) * mapX + int(floor((xPos) / tilleWidth))] == 0 && map[int((floor(yPos - pdy) / tilleWidth)) * mapX + int(floor((xPos + 32) / tilleWidth))] == 0 && map[int((floor(yPos - pdy + 32) / tilleWidth)) * mapX + int(floor((xPos + 32) / tilleWidth))] == 0 && map[int((floor(yPos - pdy + 32) / tilleWidth)) * mapX + int(floor((xPos) / tilleWidth))] == 0) {
 				yPos -= pdy;
 			}
-			if (map[int((floor(yPos) / w_tille)) * mapX + int(floor((xPos - pdx) / w_tille))] == 0 && map[int((floor(yPos) / w_tille)) * mapX + int(floor((xPos - pdx + 32) / w_tille))] == 0 && map[int((floor(yPos + 32) / w_tille)) * mapX + int(floor((xPos - pdx + 32) / w_tille))] == 0 && map[int((floor(yPos + 32) / w_tille)) * mapX + int(floor((xPos - pdx) / w_tille))] == 0) {
+			if (map[int((floor(yPos) / tilleWidth)) * mapX + int(floor((xPos - pdx) / tilleWidth))] == 0 && map[int((floor(yPos) / tilleWidth)) * mapX + int(floor((xPos - pdx + 32) / tilleWidth))] == 0 && map[int((floor(yPos + 32) / tilleWidth)) * mapX + int(floor((xPos - pdx + 32) / tilleWidth))] == 0 && map[int((floor(yPos + 32) / tilleWidth)) * mapX + int(floor((xPos - pdx) / tilleWidth))] == 0) {
 				xPos -= pdx;
 			}
 		}
@@ -222,248 +451,13 @@ public:
 	}
 };
 
-double distance(double px, double py, double rx, double ry, double ra, double pa) {
-	double distance = 0;
-
-	double x = rx - px;
-	double y = ry - py;
-
-	distance = x * cos(pa) + y * sin(pa);
-
-	return distance;
-}
-
-void raycasting(int mapX, int mapY, int mapS, int* map, double xPos, double yPos, double pa, SDL_Renderer* renderer, int w_tille, int& currentFrame) {
-	double ry = -1, rx = -1;
-
-	double hy = 0, hx = 0;
-	double vy = 0, vx = 0;
-	double hd = 0, vd = 0;
-
-	double fd = 0;
-
-	double xo = 0, yo = 0;
-
-	double px = xPos + 16;
-	double py = yPos + 16;
-
-	double dx = 0, dy = 0;
-
-	int mx, my, mp;
-
-	int dof;
-
-	double ra = pa;
-
-	int rays = 100;
-
-	ra -= rays / 2 * RAD;
-
-
-	SDL_SetRenderDrawColor(renderer, 200, 20, 20, 255);
-	SDL_Rect rect = { WIDTH, 800, WIDTH, -HEIGHT / 2 };
-	SDL_RenderFillRect(renderer, &rect);
-
-	for (int r = 0; r < rays; r++) {
-		bool horizontalHit;
-		if (ra < 0) {
-			ra += 2 * PI;
-		}
-		if (ra > 2 * PI) {
-			ra -= 2 * PI;
-		}
-		// Check horizontal lines
-		dof = 0;
-		double aTan = -1 / tan(ra);
-
-		// looking straight left or right
-		if (ra <= 0 || ra == PI) {
-			rx = px; ry = py;
-			dof = 30;
-		}
-
-		// looking up
-		if (ra > PI) {
-			ry = floor(py / w_tille) * w_tille - 1;
-			dy = py - ry;
-			rx = (dy)*aTan + px;
-			yo = -w_tille;
-			xo = -yo * aTan;
-		}
-
-		// looking down
-		if (ra < PI) {
-			ry = floor(py / w_tille) * w_tille + w_tille;
-			dy = py - ry;
-			rx = (dy)*aTan + px;
-			yo = w_tille;
-			xo = -yo * aTan;
-		}
-
-
-		while (dof < 30) {
-			mx = int(floor(rx / w_tille));
-			my = int(floor(ry / w_tille));
-			mp = my * mapX + mx;
-
-			// hit wall
-			if (mp > 0 && mp < mapX * mapY && map[mp] != 0) {
-				dof = 30;
-				break;
-			}
-			else {
-				dof++;
-				rx += xo;
-				ry += yo;
-			}
-		}
-
-		hy = ry, hx = rx;
-		hd = distance(px, py, hx, hy, ra, pa);
-
-		// Check vertical lines
-
-		dof = 0;
-		double nTan = -tan(ra);
-
-		// looking straight up or down
-		if (ra == PI3 || ra == PI2) {
-			rx = px; ry = py;
-			dof = 30;
-		}
-
-		// looking right
-		if (ra < PI2 || ra > PI3) {
-			rx = floor(px / w_tille) * w_tille + w_tille;
-			dx = px - rx;
-			ry = (dx)*nTan + py;
-			xo = w_tille;
-			yo = -xo * nTan;
-		}
-
-		// looking left
-		if (ra > PI2 && ra < PI3) {
-			rx = floor(px / w_tille) * w_tille - 1;
-			dx = px - rx;
-			ry = (dx)*nTan + py;
-			xo = -w_tille;
-			yo = -xo * nTan;
-		}
-
-		while (dof < 30) {
-			mx = int(floor(rx / w_tille));
-			my = int(floor(ry / w_tille));
-			mp = my * mapX + mx;
-
-			// hit wall
-			if (mp > 0 && mp < mapX * mapY && map[mp] != 0) {
-				dof = 30;
-				break;
-			}
-			else {
-				dof++;
-				rx += xo;
-				ry += yo;
-			}
-		}
-
-		vy = ry, vx = rx;
-		vd = distance(px, py, vx, vy, ra, pa);
-
-		if (hd > vd) {
-			rx = vx;
-			ry = vy;
-			fd = vd;
-			horizontalHit = false;
-		}
-		else if (hd < vd) {
-			rx = hx;
-			ry = hy;
-			fd = hd;
-			horizontalHit = true;
-		}
-		else {
-			// Both distances are the same; handle corner/edge case
-			// You can choose which wall to render based on ray direction
-			if (ra >= 0 && ra < PI) {
-				// Ray is facing down, prefer vertical hit
-				rx = hx;
-				ry = hy;
-				fd = hd;
-				horizontalHit = true;
-			}
-			else {
-				// Ray is facing up, prefer horizontal hit
-				rx = vx;
-				ry = vy;
-				fd = vd;
-				horizontalHit = false;
-			}
-		}
-
-		if (rx >= 0 && rx <= 800) {
-			if (ry >= 0 && ry <= 800) {
-				mx = int(floor(rx / w_tille));
-				my = int(floor(ry / w_tille));
-				mp = my * mapX + mx;
-
-				// hit wall
-				if (mp > 0 && mp < mapX * mapY) {
-					if (map[mp] == 1) {
-						SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-					}
-					else if (map[mp] == 2) {
-						SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-					}
-					else {
-						SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-					}
-					SDL_RenderDrawLine(renderer, px, py, rx, ry);
-				}
-
-				// Draw 3D Walls
-				double wh = (w_tille * HEIGHT) / fd;
-				if (wh > 1000) {
-					wh = 1000;
-				}
-
-				double lineO = HEIGHT - wh / 1.5;
-
-				SDL_Rect rect = { r * (800 / rays) + WIDTH, lineO / 2, 800 / rays, wh };
-				if (mp > 0 && mp < mapX * mapY) {
-					if (map[mp] == 1) {
-						if (horizontalHit == true) {
-							SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-						}
-						else if (horizontalHit == false) {
-							SDL_SetRenderDrawColor(renderer, 0, 0, 130, 255);
-						}
-						else { continue; }
-					}
-					else if (map[mp] == 2) {
-						if (horizontalHit==true) {
-							SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-						}
-						else if (horizontalHit == false) {
-							SDL_SetRenderDrawColor(renderer, 0, 130, 0, 255);
-						}
-					}
-					SDL_RenderFillRect(renderer, &rect);
-				}
-			}
-		}
-
-		ra += RAD;
-	}
-}
-
 int main(int argc, char** argv){
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	Game game;
 	Player player;
 
-	game.MakeWindow();
+	game.MakeWindow("Game");
 
 	player.Update(game.renderer);
 
@@ -471,7 +465,7 @@ int main(int argc, char** argv){
 	while (game.isRunning) {
 		game.HandleFps();
 
-		player.Input(game.isRunning, game.w_tille, game.mapX, game.mapY, game.mapS, game.map); // Movement, QUIT
+		player.Input(game.isRunning, game.tilleWidth, game.mapX, game.mapY, game.mapSize, game.map); // Movement, QUIT
 
 		SDL_RenderClear(game.renderer);
 
@@ -479,11 +473,7 @@ int main(int argc, char** argv){
 
 		player.Draw(game.renderer);
 
-
-		/*SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-		SDL_RenderDrawLine(renderer, xPos + 16, yPos + 16, xPos + 16 + pdx*50, yPos + 16 + pdy*50);*/
-
-		raycasting(game.mapX, game.mapY, game.mapS, game.map, player.xPos, player.yPos, player.pa, game.renderer, game.w_tille, player.currentFrame);
+		game.raycasting(player.xPos, player.yPos, player.playerAngle, player.currentFrame);
 
 		SDL_SetRenderDrawColor(game.renderer, 200, 200, 200, 255);
 
